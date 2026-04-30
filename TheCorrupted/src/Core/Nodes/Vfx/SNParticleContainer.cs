fdd8fafcabef05b42e13b31abb9566938be028ed
LeifererGamer <100093491+LeifererGamer@users.noreply.github.com>
2026-04-30 21:41:34 +0200

    Added Epochs and Patches
    Added Epochs and Patches, so that the game wont crash, when killing an Elite, Epochs still not 100% working

---
+++ TheCorrupted/src/Core/Nodes/Vfx/SNParticleContainer.cs
@@ -0,0 +1,13 @@
+﻿using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCorrupted.TheCorrupted.src.Core.Nodes.Vfx
+{
+    public partial class SNParticleContainer : NParticlesContainer
+    {
+    }
+}
