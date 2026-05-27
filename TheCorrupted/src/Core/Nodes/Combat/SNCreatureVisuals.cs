338d88e6156e9626051dab395c88f106a9cf186c
LeifererGamer <100093491+LeifererGamer@users.noreply.github.com>
2026-05-27 11:28:24 +0200

    Update SNCreatureVisuals.cs

--- TheCorrupted/src/Core/Nodes/Combat/SNCreatureVisuals.cs
+++ TheCorrupted/src/Core/Nodes/Combat/SNCreatureVisuals.cs
@@ -27,7 +27,6 @@ public partial class SNCreatureVisuals : NCreatureVisuals
 
     public void _on_animation_player_animation_finished(StringName anim_name)
     {
-        ModEntry.Logger.Info($"Animation finished: {anim_name}");
         if (anim_name != "idle")
         {
             _anim?.Play("idle");
